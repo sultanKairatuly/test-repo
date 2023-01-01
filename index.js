bebb5cfd3bfaf68ed327357b85320b588d637cef
SultanbekKajratuly <sultanbekkajratuly@gmail.com>
2023-01-01 10:37:34 +0600

    made some changes

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ setTimeout(() => {
   console.log("Hello");
 }, 1000);
 console.log("this change is from test branch");
-function multiply(a, b = a) {
+function multiply(a = b, b = a) {
+  console.log("a: ", a);
+  console.log("b: ", b);
   return a * b;
 }
 
-const sum = multiply(5);
+const sum = multiply(5, 6);
 console.log(sum);
+console.log("dddd");
